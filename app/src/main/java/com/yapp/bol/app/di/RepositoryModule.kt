824fdfc63abd90c6576d6f72ab2ed2da4b558e4c
EunJung Jung <agjjung@naver.com>
2023-05-26 20:11:07 +0900

    refactor: @Provide 주입 부분 @Binds로 리팩터링

--- app/src/main/java/com/yapp/bol/app/di/RepositoryModule.kt
+++ app/src/main/java/com/yapp/bol/app/di/RepositoryModule.kt
@@ -1,24 +1,17 @@
 package com.yapp.bol.app.di
 
-import com.yapp.bol.data.datasource.impl.MockDataSourceImpl
 import com.yapp.bol.data.repository.MockRepositoryImpl
 import com.yapp.bol.domain.repository.MockRepository
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class RepositoryModule {
-    @Provides
+abstract class RepositoryModule {
+    @Binds
     @Singleton
-    fun provideMockRepository(
-        mockDataSourceImpl: MockDataSourceImpl,
-    ): MockRepository {
-        return MockRepositoryImpl(
-            mockDataSourceImpl,
-        )
-    }
+    abstract fun bindMockRepository(mockRepositoryImpl: MockRepositoryImpl): MockRepository
 }
