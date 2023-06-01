b724b82351d6fcd9c1f5fd588aeceab734a2d5d6
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-06-01 10:23:51 +0900

    feat: ActivityKakaoTestBinding  추가

--- presentation/src/main/java/com/yapp/bol/presentation/view/login/KakaoTestActivity.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/login/KakaoTestActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.yapp.bol.presentation.R
 import com.yapp.bol.presentation.BuildConfig
+import com.yapp.bol.presentation.databinding.ActivityKakaoTestBinding
 import com.yapp.bol.presentation.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class KakaoTestActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityKakaoTestBinding
 
     private val mainViewModel: MainViewModel by viewModels()
 
