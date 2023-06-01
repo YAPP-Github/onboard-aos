7847f5fa46f437ebd14f564414a2f2c22ee621f2
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-06-01 10:30:09 +0900

    feat: 설치가 되어있을 때 실행하는 함수 추가

--- presentation/src/main/java/com/yapp/bol/presentation/view/login/KakaoTestActivity.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/login/KakaoTestActivity.kt
@@ -3,8 +3,11 @@ package com.yapp.bol.presentation.view.login
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.yapp.bol.presentation.R
+import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import com.yapp.bol.presentation.BuildConfig
 import com.yapp.bol.presentation.databinding.ActivityKakaoTestBinding
 import com.yapp.bol.presentation.viewmodel.MainViewModel
@@ -37,6 +40,14 @@ class KakaoTestActivity : AppCompatActivity() {
 
         KakaoSdk.init(this, KAKAO_API_KEY)
         kakaoClient = UserApiClient.instance
+    private fun handleKakaoTalkLoginResult() {
+        kakaoClient.loginWithKakaoTalk(this) { token, error ->
+            if (isClientErrorCancelled(error)) return@loginWithKakaoTalk
+            error?.let { kakaoClient.loginWithKakaoAccount(this, callback = kakaoCallBack) }
+            token?.let { mainViewModel.loginTest(token.accessToken) }
+        }
+    }
+
     companion object {
         const val KAKAO_API_KEY = BuildConfig.KAKAO_API_KEY
     }
