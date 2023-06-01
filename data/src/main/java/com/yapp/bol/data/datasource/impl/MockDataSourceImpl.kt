9677a579fc480d8da586059181679f8085f1531f
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-06-01 10:15:19 +0900

    feat: MockType 이름 변경 및 토큰 값 넘기기

--- data/src/main/java/com/yapp/bol/data/datasource/impl/MockDataSourceImpl.kt
+++ data/src/main/java/com/yapp/bol/data/datasource/impl/MockDataSourceImpl.kt
@@ -3,7 +3,7 @@ package com.yapp.bol.data.datasource.impl
 import com.yapp.bol.data.datasource.MockDataSource
 import com.yapp.bol.data.model.MockApiRequest
 import com.yapp.bol.data.model.MockApiResponse
-import com.yapp.bol.data.model.MockType
+import com.yapp.bol.data.model.LoginType
 import com.yapp.bol.data.remote.LoginApi
 import com.yapp.bol.data.utils.BaseRepository
 import com.yapp.bol.domain.utils.RemoteErrorEmitter
@@ -14,7 +14,7 @@ class MockDataSourceImpl @Inject constructor(
 ) : BaseRepository(), MockDataSource {
     override suspend fun getKakaoMock(remoteErrorEmitter: RemoteErrorEmitter, token: String): MockApiResponse? {
         return safeApiCall(remoteErrorEmitter) {
-            oauthApi.postMockApi(MockApiRequest(MockType.KAKAO, "SUCCESSid")).body()
+            oauthApi.postMockApi(MockApiRequest(LoginType.KAKAO_ACCESS_TOKEN, token)).body()
         }
     }
 }
--- data/src/main/java/com/yapp/bol/data/model/MockApiRequest.kt
+++ data/src/main/java/com/yapp/bol/data/model/MockApiRequest.kt
@@ -2,15 +2,15 @@ package com.yapp.bol.data.model
 
 import com.google.gson.annotations.SerializedName
 
-enum class MockType(val type: String) {
-    KAKAO("KAKAO"),
-    GOOGLE("GOOGLE"),
-    NAVER("NVER"),
+enum class LoginType {
+    KAKAO_ACCESS_TOKEN,
+    GOOGLE,
+    NAVER_ACCESS_TOKEN,
 }
 
 data class MockApiRequest(
     @SerializedName("type")
-    val type: MockType,
+    val type: LoginType,
     @SerializedName("token")
     val token: String,
 )
