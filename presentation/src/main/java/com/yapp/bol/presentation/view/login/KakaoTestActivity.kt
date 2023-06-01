3090d50128712897541743ab27b7b89438c5ba1b
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-06-01 10:23:32 +0900

    feat: ViewModel 추가 및 AndroidEntryPoint 추가

--- presentation/src/main/java/com/yapp/bol/presentation/view/login/KakaoTestActivity.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/login/KakaoTestActivity.kt
@@ -1,10 +1,18 @@
 package com.yapp.bol.presentation.view.login
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.yapp.bol.presentation.R
+import com.yapp.bol.presentation.BuildConfig
+import com.yapp.bol.presentation.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class KakaoTestActivity : AppCompatActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kakao_test)
