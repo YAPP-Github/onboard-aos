259c9e885502e83e62879462d00d0637b79d781e
EunJung Jung <agjjung@naver.com>
2023-06-04 03:56:06 +0900

    BOL-11 refactor: google login lateinit 사용하지 않도록 수정

--- presentation/src/main/java/com/yapp/bol/presentation/view/login/GoogleTestActivity.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/login/GoogleTestActivity.kt
@@ -3,13 +3,11 @@ package com.yapp.bol.presentation.view.login
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
@@ -22,37 +20,32 @@ import com.yapp.bol.presentation.BuildConfig
 import com.yapp.bol.presentation.R
 
 class GoogleTestActivity : AppCompatActivity() {
+
     private val auth: FirebaseAuth by lazy { Firebase.auth }
-    private lateinit var googleLoginClient: GoogleSignInClient
-    private lateinit var googleLoginForResult: ActivityResultLauncher<Intent>
+    private val googleLoginClientIntent: Intent by lazy {
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(BuildConfig.GOOGLE_LOGIN_API_KEY)
+            .requestEmail()
+            .build()
+        GoogleSignIn.getClient(this, gso).signInIntent
+    }
+    private val googleLoginForResult: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            handleGoogleSignInResult(result)
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_google_test)
 
-        initGoogleLogin()
-
         // TEST 용이라 binding 연결해두지 않았습니다.
         findViewById<SignInButton>(R.id.btn_login).setOnClickListener {
-            val signInIntent = googleLoginClient.signInIntent
-            googleLoginForResult.launch(signInIntent)
+            googleLoginForResult.launch(googleLoginClientIntent)
         }
     }
 
-    private fun initGoogleLogin() {
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(BuildConfig.GOOGLE_LOGIN_API_KEY)
-            .requestEmail()
-            .build()
-
-        googleLoginClient = GoogleSignIn.getClient(this, gso)
-
-        googleLoginForResult =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
-                handleGoogleSignInResult(result)
-            }
-    }
-
+    // 구글 로그인 작업 수행
     private fun handleGoogleSignInResult(result: ActivityResult) {
         if (result.resultCode == RESULT_OK) {
             result.data?.let { data ->
@@ -70,26 +63,25 @@ class GoogleTestActivity : AppCompatActivity() {
         }
     }
 
+    // 구글 로그인에 성공한 아이디를 Firebase에 통신하여 user 정보 가져오기
     private fun startFirebaseAuthWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         auth.signInWithCredential(credential)
             .addOnCompleteListener(this) { task ->
                 val user = auth.currentUser
                 if (task.isSuccessful && user != null) {
-                    Log.d("gso check uid", user.uid)
-                    checkFirebaseIdToken(auth.currentUser)
+                    checkFirebaseIdToken(user)
                 } else {
                     // TODO : If sign in fails, display a message to the user.
                 }
             }
     }
 
-    private fun checkFirebaseIdToken(user: FirebaseUser?) {
-        user?.getIdToken(true)?.addOnSuccessListener {
+    // 가져온 user 정보로 idToken 가져오는 작업 수행
+    private fun checkFirebaseIdToken(user: FirebaseUser) {
+        user.getIdToken(true).addOnSuccessListener {
             Log.d("gso check idToken", it.token ?: "null")
-            // TODO 최종 성공 작업
-        } ?: kotlin.run {
-            // TODO 예외 작업
+            // TODO 최종 성공 작업 -> login 성공 & 서버로 토큰 보내기 (BOL-11-2 브랜치에서 작업할 예정)
         }
     }
 }
