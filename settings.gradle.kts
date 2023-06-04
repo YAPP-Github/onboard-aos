fd3eda803274921142f41bda8de940466641b25e
KyeongMIn <goodluckplz820@gmail.com>
2023-06-05 01:49:43 +0900

    BOL-15 style : ktlint code convection & 사용하지 않는 파일 제거

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,12 +13,12 @@ dependencyResolutionManagement {
         mavenCentral()
         jcenter() // Warning: this repository is going to shut down soon
         maven { url = uri("https://jitpack.io") }
-        maven { url = uri("https://devrepo.kakao.com/nexus/content/groups/public/")}
+        maven { url = uri("https://devrepo.kakao.com/nexus/content/groups/public/") }
     }
 }
 
 rootProject.name = "22nd-Android-Team-2-Android"
-include (":app")
-include (":domain")
-include (":data")
-include (":presentation")
+include(":app")
+include(":domain")
+include(":data")
+include(":presentation")
