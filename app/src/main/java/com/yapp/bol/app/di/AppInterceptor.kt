cd8cdfa044c5f118cb87ccca537e5ec988a92e24
jaehong <83493143+prk4224@users.noreply.github.com>
2023-06-07 14:15:42 +0900

    refactor: Interceptor  에 ContenetType 제거

--- app/src/main/java/com/yapp/bol/app/di/AppInterceptor.kt
+++ app/src/main/java/com/yapp/bol/app/di/AppInterceptor.kt
@@ -6,7 +6,6 @@ import okhttp3.Response
 class AppInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response = with(chain) {
         val newRequest = request().newBuilder()
-            .addHeader("Content-Type", "application/json")
             .build()
         proceed(newRequest)
     }
