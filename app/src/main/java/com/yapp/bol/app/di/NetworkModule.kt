6946003da53a81396c11dcbafaaa14882de09b2e
KyeongMIn <goodluckplz820@gmail.com>
2023-06-10 16:25:27 +0900

    feat :  accessToken 만료시 refreshToken 교체 작업 기능 추가

--- app/src/main/java/com/yapp/bol/app/di/NetworkModule.kt
+++ app/src/main/java/com/yapp/bol/app/di/NetworkModule.kt
@@ -34,13 +34,13 @@ object NetworkModule {
     @Singleton
     fun provideHttpClient(
         httpLoggingInterceptor: HttpLoggingInterceptor,
-        appInterceptor: AppInterceptor,
+        authInterceptor: AuthInterceptor
     ): OkHttpClient {
         return OkHttpClient.Builder()
             .readTimeout(10, TimeUnit.SECONDS)
             .connectTimeout(10, TimeUnit.SECONDS)
             .writeTimeout(15, TimeUnit.SECONDS)
-            .addInterceptor(appInterceptor)
+            .addInterceptor(authInterceptor)
             .addInterceptor(httpLoggingInterceptor)
             .build()
     }
@@ -49,7 +49,7 @@ object NetworkModule {
     @Provides
     fun provideRetrofitInstance(
         okHttpClient: OkHttpClient,
-        gsonConverterFactory: GsonConverterFactory
+        gsonConverterFactory: GsonConverterFactory,
     ): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
@@ -69,8 +69,4 @@ object NetworkModule {
     fun provideOAuthApiService(retrofit: Retrofit): LoginApi {
         return retrofit.create(LoginApi::class.java)
     }
-
-    @Provides
-    @Singleton
-    fun provideAppInterceptor(): AppInterceptor = AppInterceptor()
 }
