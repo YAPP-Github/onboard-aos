b3daad393b4963cd7d3ded6c7cb4c0ef21367204
jaehong <83493143+prk4224@users.noreply.github.com>
2023-06-12 11:18:03 +0900

    refactor: Code Clean Up

--- presentation/src/main/java/com/yapp/bol/presentation/viewmodel/KakaoTestViewModel.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/viewmodel/KakaoTestViewModel.kt
@@ -1,15 +1,14 @@
 package com.yapp.bol.presentation.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.yapp.bol.domain.model.MockApiItem
+import com.yapp.bol.domain.model.OAuthApiItem
 import com.yapp.bol.domain.usecase.login.KakaoLoginUseCase
-import com.yapp.bol.domain.utils.ErrorType
-import com.yapp.bol.domain.utils.RemoteErrorEmitter
 import com.yapp.bol.presentation.utils.Constant.EMPTY_STRING
+import com.yapp.bol.presentation.utils.checkedApiResult
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,21 +16,24 @@ import javax.inject.Inject
 @HiltViewModel
 class KakaoTestViewModel @Inject constructor(
     private val kakaoLoginUseCase: KakaoLoginUseCase,
-) : ViewModel(), RemoteErrorEmitter {
+) : ViewModel() {
 
     private val _accessToken = MutableLiveData(EMPTY_STRING)
     val accessToken = _accessToken
 
-    fun loginTest(token: String) = viewModelScope.launch {
-        val response: MockApiItem? = kakaoLoginUseCase.execute(this@KakaoTestViewModel, token)
-        _accessToken.value = response?.accessToken ?: EMPTY_STRING
+    fun loginTest(token: String) {
+        viewModelScope.launch {
+            kakaoLoginUseCase(token).collectLatest {
+                checkedApiResult(
+                    apiResult = it,
+                    success = ::updateAccessToken,
+                    error = { throwable -> throw throwable }
+                )
+            }
+        }
     }
 
-    override fun onError(msg: String) {
-        Log.d("MOCK TEST", msg)
-    }
-
-    override fun onError(errorType: ErrorType) {
-        Log.d("MOCK TEST", errorType.name)
+    private fun updateAccessToken(item: OAuthApiItem) {
+        accessToken.value = item.accessToken
     }
 }
