8d81ac40773cc217b5d8d3669968c9356c1513c5
EunJung Jung <agjjung@naver.com>
2023-06-12 21:04:10 +0900

    BOL-61 style: ktlint 맞춰서 코드 스타일 수정

--- presentation/src/main/java/com/yapp/bol/presentation/view/group/search/GroupSearchFragment.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/group/search/GroupSearchFragment.kt
@@ -31,7 +31,8 @@ class GroupSearchFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
