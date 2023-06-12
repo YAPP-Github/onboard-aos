366ad6f38aeabb56e637718ec8b115242806d937
jaehong <83493143+prk4224@users.noreply.github.com>
2023-06-12 11:01:50 +0900

    refactor: 오타 수정

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
         buildConfigField("String", "KAKAO_API_KEY", getApi("KAKAO_API_KEY"))
-        manifestPlaceholders["kakaoKay"] = getApi("KAKAO_API_KEY_MANI")
+        manifestPlaceholders["kakaoKey"] = getApi("KAKAO_API_KEY_MANI")
     }
 
     buildTypes {
