2fc5962b833a5a26499dcc4c5bdb3e890ef59dc2
jaehong <83493143+prk4224@users.noreply.github.com>
2023-06-12 11:16:37 +0900

    refactor: safeApiCall 재구성

--- data/src/main/java/com/yapp/bol/data/utils/BaseRepository.kt
+++ data/src/main/java/com/yapp/bol/data/utils/BaseRepository.kt
@@ -1,97 +1,30 @@
 package com.yapp.bol.data.utils
 
-import android.util.Log
-import com.yapp.bol.domain.utils.ErrorType
-import com.yapp.bol.domain.utils.RemoteErrorEmitter
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import okhttp3.ResponseBody
-import org.json.JSONObject
-import retrofit2.HttpException
+import com.yapp.bol.domain.model.ApiResult
+import retrofit2.Response
 import java.io.IOException
-import java.net.SocketTimeoutException
 
 abstract class BaseRepository {
+    suspend fun <T : Any> safeApiCall(callFunction: suspend () -> Response<T>): ApiResult<T> {
+        val response: Response<T>
 
-    companion object {
-        private const val TAG = "BaseRemoteRepository"
-        private const val MESSAGE_KEY = "message"
-        private const val ERROR_KEY = "error"
-    }
-
-    /**
-     * Function that executes the given function on Dispatchers.IO context and switch to Dispatchers.Main context when an error occurs
-     * @param callFunction is the function that is returning the wanted object. It must be a suspend function. Eg:
-     * override suspend fun loginUser(body: LoginUserBody, emitter: RemoteErrorEmitter): LoginUserResponse?  = safeApiCall( { authApi.loginUser(body)} , emitter)
-     * @param emitter is the interface that handles the error messages. The error messages must be displayed on the MainThread, or else they would throw an Exception.
-     */
-    suspend inline fun <T> safeApiCall(emitter: RemoteErrorEmitter, crossinline callFunction: suspend () -> T): T? {
-        return try {
-            val myObject = withContext(Dispatchers.IO) { callFunction.invoke() }
-            myObject
+        try {
+            response = callFunction()
         } catch (e: Exception) {
-            withContext(Dispatchers.Main) {
-                e.printStackTrace()
-                Log.e("BaseRemoteRepo", "Call error: ${e.localizedMessage}", e.cause)
-                when (e) {
-                    is HttpException -> {
-                        if (e.code() == 401) {
-                            emitter.onError(ErrorType.SESSION_EXPIRED)
-                        } else {
-                            val body = e.response()?.errorBody()
-                            emitter.onError(getErrorMessage(body))
-                        }
-                    }
-                    is SocketTimeoutException -> emitter.onError(ErrorType.TIMEOUT)
-                    is IOException -> emitter.onError(ErrorType.NETWORK)
-                    else -> emitter.onError(ErrorType.UNKNOWN)
+            return when (e) {
+                is IOException -> {
+                    ApiResult.Error(IOException(e.message ?: "Internet error runs"))
                 }
-            }
-            null
-        }
-    }
 
-    /**
-     * Function that executes the given function in whichever thread is given. Be aware, this is not friendly with Dispatchers.IO,
-     * since [RemoteErrorEmitter] is intended to display messages to the user about error from the server/DB.
-     * @param callFunction is the function that is returning the wanted object. Eg:
-     * override suspend fun loginUser(body: LoginUserBody, emitter: RemoteErrorEmitter): LoginUserResponse?  = safeApiCall( { authApi.loginUser(body)} , emitter)
-     * @param emitter is the interface that handles the error messages. The error messages must be displayed on the MainThread, or else they would throw an Exception.
-     */
-    inline fun <T> safeApiCallNoContext(emitter: RemoteErrorEmitter, callFunction: () -> T): T? {
-        return try {
-            val myObject = callFunction.invoke()
-            myObject
-        } catch (e: Exception) {
-            e.printStackTrace()
-            Log.e("BaseRemoteRepo", "Call error: ${e.localizedMessage}", e.cause)
-            when (e) {
-                is HttpException -> {
-                    if (e.code() == 401) {
-                        emitter.onError(ErrorType.SESSION_EXPIRED)
-                    } else {
-                        val body = e.response()?.errorBody()
-                        emitter.onError(getErrorMessage(body))
-                    }
-                }
-                is SocketTimeoutException -> emitter.onError(ErrorType.TIMEOUT)
-                is IOException -> emitter.onError(ErrorType.NETWORK)
-                else -> emitter.onError(ErrorType.UNKNOWN)
+                else -> ApiResult.Error(e)
             }
-            null
         }
-    }
 
-    fun getErrorMessage(responseBody: ResponseBody?): String {
-        return try {
-            val jsonObject = JSONObject(responseBody!!.string())
-            when {
-                jsonObject.has(MESSAGE_KEY) -> jsonObject.getString(MESSAGE_KEY)
-                jsonObject.has(ERROR_KEY) -> jsonObject.getString(ERROR_KEY)
-                else -> "Something wrong happened"
-            }
-        } catch (e: Exception) {
-            "Something wrong happened"
+        val body = response.body()
+        return if (body != null) {
+            ApiResult.Success(body)
+        } else {
+            ApiResult.Error(IOException("Body Null Error"))
         }
     }
 }
