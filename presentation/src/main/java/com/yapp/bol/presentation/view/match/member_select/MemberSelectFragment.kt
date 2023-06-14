6a5719df039b518b7d1be8e2dac6f9ea35a428f3
jaehong <83493143+prk4224@users.noreply.github.com>
2023-06-14 16:54:43 +0900

    feat:MemberSelectFragment 구현

--- presentation/src/main/java/com/yapp/bol/presentation/view/match/member_select/MemberSelectFragment.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/match/member_select/MemberSelectFragment.kt
@@ -1,15 +1,17 @@
 package com.yapp.bol.presentation.view.match.member_select
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.yapp.bol.presentation.databinding.FragmentMemberSelectBinding
+import com.yapp.bol.presentation.utils.collectWithLifecycle
 import com.yapp.bol.presentation.view.match.MatchViewModel
 import com.yapp.bol.presentation.view.match.game_select.GameSelectFragment.Companion.GAME_NAME
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.filterNotNull
 
 @AndroidEntryPoint
 class MemberSelectFragment : Fragment() {
@@ -18,6 +20,15 @@ class MemberSelectFragment : Fragment() {
     private val binding get() = checkNotNull(_binding)
 
     private val viewModel: MatchViewModel by activityViewModels()
+    private val memberSelectAdapter = MemberSelectAdapter { member ->
+        viewModel.removeSelectMembers(member)
+    }
+    private val membersAdapter by lazy {
+        MembersAdapter(requireContext()) { member, isChecked ->
+            if (isChecked) viewModel.addSelectMembers(member)
+            else viewModel.removeSelectMembers(member)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,6 +41,20 @@ class MemberSelectFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val gameName = arguments?.getString(GAME_NAME) ?: ""
         viewModel.updateToolBarTitle(gameName)
+
+        binding.rvMemberSelect.adapter = memberSelectAdapter
+        binding.rvMembers.adapter = membersAdapter
+
+        viewModel.members.observe(viewLifecycleOwner) { members ->
+            membersAdapter.submitList(members)
+        }
+
+        viewModel.getMembers()
+        viewModel.selectMembers.filterNotNull().collectWithLifecycle(this) {
+            memberSelectAdapter.submitList(it)
+        }
+
+        memberSelectAdapter.setCallback(membersAdapter)
     }
 
     override fun onDestroyView() {
