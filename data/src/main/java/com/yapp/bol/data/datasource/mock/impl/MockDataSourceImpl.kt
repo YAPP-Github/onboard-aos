149caf127c66f275ced35ad831f6292042abe09f
EunJung Jung <agjjung@naver.com>
2023-06-15 06:43:01 +0900

    BOL-62 fix: login 관련 에러 수정 (review 필요)

--- data/src/main/java/com/yapp/bol/data/datasource/mock/impl/MockDataSourceImpl.kt
+++ data/src/main/java/com/yapp/bol/data/datasource/mock/impl/MockDataSourceImpl.kt
@@ -6,6 +6,7 @@ import com.yapp.bol.data.model.MockApiRequest
 import com.yapp.bol.data.model.MockApiResponse
 import com.yapp.bol.data.remote.LoginApi
 import com.yapp.bol.data.utils.BaseRepository
+import com.yapp.bol.domain.model.ApiResult
 import com.yapp.bol.domain.utils.RemoteErrorEmitter
 import javax.inject.Inject
 
@@ -18,8 +19,12 @@ class MockDataSourceImpl @Inject constructor(
         type: String,
         token: String,
     ): MockApiResponse? {
-        return safeApiCall(emitter) {
-            api.postMockApi(MockApiRequest(type.toDomain(), token)).body()
+        // TODO : 경민님 작업사항. 체크 필요함.
+        val temp = safeApiCall {
+            api.postMockApi(MockApiRequest(type.toDomain(), token))
+        }
+        return if (temp is ApiResult.Success) temp.data else {
+            throw Exception()
         }
     }
 }
