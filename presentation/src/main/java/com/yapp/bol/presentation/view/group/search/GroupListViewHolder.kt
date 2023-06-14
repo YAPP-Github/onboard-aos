eeb04e3520f86e1a4b7ea0935aaa9c1dfd5c4f5e
EunJung Jung <agjjung@naver.com>
2023-06-15 01:09:42 +0900

    BOL-62 feat: Group List VH의 item들 domain에 구현한 item으로 변경

--- presentation/src/main/java/com/yapp/bol/presentation/view/group/search/GroupListViewHolder.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/group/search/GroupListViewHolder.kt
@@ -5,10 +5,11 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.yapp.bol.domain.model.GroupSearchItem
 import com.yapp.bol.presentation.databinding.ItemGroupListBinding
 
 class GroupListViewHolder(private val binding: ItemGroupListBinding) : RecyclerView.ViewHolder(binding.root) {
-    private var groupItem: String? = null
+    private var groupItem: GroupSearchItem? = null
     private val glide by lazy { Glide.with(binding.root) }
 
     init {
@@ -17,7 +18,7 @@ class GroupListViewHolder(private val binding: ItemGroupListBinding) : RecyclerV
         }
     }
 
-    fun bind(groupItem: String?) {
+    fun bind(groupItem: GroupSearchItem?) {
         if (groupItem == null) {
             // TODO ERROR
         } else {
@@ -25,7 +26,7 @@ class GroupListViewHolder(private val binding: ItemGroupListBinding) : RecyclerV
         }
     }
 
-    private fun showGroupData(groupItem: String) {
+    private fun showGroupData(groupItem: GroupSearchItem) {
         this.groupItem = groupItem
 
         binding.apply {
