35bb0aff9e13bf368c808132dcf29a787ce663f5
KyeongMIn <goodluckplz820@gmail.com>
2023-06-14 20:08:28 +0900

    BOL-10 refactor : property 이름 변경
    dataStore -> authManager

--- data/src/main/java/com/yapp/bol/data/datasource/auth/impl/AuthDataSourceImpl.kt
+++ data/src/main/java/com/yapp/bol/data/datasource/auth/impl/AuthDataSourceImpl.kt
@@ -8,18 +8,18 @@ import javax.inject.Singleton
 
 @Singleton
 class AuthDataSourceImpl @Inject constructor(
-    private val dataStore: AuthManager,
+    private val authManager: AuthManager,
 ) : AuthDataSource {
 
-    override val accessToken: Flow<String> = dataStore.accessKey
+    override val accessToken: Flow<String> = authManager.accessKey
 
-    override val refreshToken: Flow<String> = dataStore.refreshKey
+    override val refreshToken: Flow<String> = authManager.refreshKey
 
     override suspend fun saveAccessToken(token: String) {
-        dataStore.saveAccessToken(token)
+        authManager.saveAccessToken(token)
     }
 
     override suspend fun saveRefreshToken(token: String) {
-        dataStore.saveRefreshToken(token)
+        authManager.saveRefreshToken(token)
     }
 }
