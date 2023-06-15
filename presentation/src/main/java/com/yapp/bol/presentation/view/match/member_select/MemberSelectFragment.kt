ac364f2bc3cc47dfdf3468ab24b5d3e7b8a6e6e7
jaehong <83493143+prk4224@users.noreply.github.com>
2023-06-15 18:49:37 +0900

    feat: 멤버 스크롤 시 키보드 내려가는 기능 & 코드 클린 업

--- presentation/src/main/java/com/yapp/bol/presentation/view/match/member_select/MemberSelectFragment.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/match/member_select/MemberSelectFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.yapp.bol.presentation.databinding.FragmentMemberSelectBinding
 import com.yapp.bol.presentation.utils.collectWithLifecycle
 import com.yapp.bol.presentation.view.match.MatchViewModel
@@ -21,13 +23,13 @@ class MemberSelectFragment : Fragment() {
     private val binding get() = checkNotNull(_binding)
 
     private val viewModel: MatchViewModel by activityViewModels()
+
     private val memberSelectAdapter = MemberSelectAdapter { member ->
-        viewModel.removeSelectMembers(member)
-        viewModel.clearMembersChecked(member.id)
+        viewModel.checkedSelectMembers(member)
+        viewModel.clearMemberChecked(member.id)
     }
-    private val membersAdapter = MembersAdapter { member, isChecked ->
-        if (isChecked) viewModel.addSelectMembers(member)
-        else viewModel.removeSelectMembers(member)
+    private val membersAdapter = MembersAdapter { member ->
+        viewModel.checkedSelectMembers(member)
     }
 
     override fun onCreateView(
@@ -56,10 +58,23 @@ class MemberSelectFragment : Fragment() {
         viewModel.isCompleteButtonEnabled.observe(viewLifecycleOwner) {
             binding.btnPlayerComplete.isEnabled = it
         }
+
+        val scrollListener = object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                hideKeyboard()
+            }
+        }
+        binding.rvMembers.addOnScrollListener(scrollListener)
+
         binding.etSearchMember.doOnTextChanged { text, start, before, count ->
             viewModel.updateSearchMembers(text.toString())
         }
     }
+
+    private fun hideKeyboard() {
+        if (activity == null && activity?.currentFocus == null) return
+        val inputManager = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(activity?.currentFocus?.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
     }
 
     override fun onDestroyView() {
