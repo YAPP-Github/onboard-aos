f6cb07c01c745d1633f061c400f73f0e41a7cc9e
jaehong <83493143+prk4224@users.noreply.github.com>
2023-06-16 08:25:22 +0900

    refactor: 불필요한 라인 제거

--- presentation/src/main/java/com/yapp/bol/presentation/view/match/game_select/GameSelectFragment.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/match/game_select/GameSelectFragment.kt
@@ -36,7 +36,6 @@ class GameSelectFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.rvProductItems.adapter = gameSelectAdapter
-
         gameSelectAdapter.submitList(testList)
     }
 
