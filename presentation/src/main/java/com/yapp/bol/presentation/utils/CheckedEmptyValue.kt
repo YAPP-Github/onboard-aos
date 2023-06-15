fa709a05a00b5391834f57c2834bf2c04012c289
jaehong <83493143+prk4224@users.noreply.github.com>
2023-06-16 08:08:55 +0900

    refactor: isEmptyValue -> isInputTextValid 변수 이름 변경

--- presentation/src/main/java/com/yapp/bol/presentation/utils/CheckedEmptyValue.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/utils/CheckedEmptyValue.kt
@@ -2,7 +2,7 @@ package com.yapp.bol.presentation.utils
 
 import com.yapp.bol.presentation.utils.Constant.EMPTY_REGEX
 
-val isEmptyValue: (String?) -> Boolean = { value ->
+val isInputTextValid: (String?) -> Boolean = { value ->
     val regex = Regex(EMPTY_REGEX)
      value?.matches(regex) ?: false
 }
