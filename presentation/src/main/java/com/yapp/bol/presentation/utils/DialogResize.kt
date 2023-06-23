5d796c3cf6f66623191831e1a531d604e1071e7f
jaehong <83493143+prk4224@users.noreply.github.com>
2023-06-23 09:27:29 +0900

    feat:   다이얼 로크 크기 변경하는 로직에서 ResultDialog 일때 고정값 사용하는 로직으로 수정

--- presentation/src/main/java/com/yapp/bol/presentation/utils/DialogResize.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/utils/DialogResize.kt
@@ -6,8 +6,13 @@ import android.graphics.Point
 import android.os.Build
 import android.view.ViewGroup
 import android.view.WindowManager
+import com.yapp.bol.presentation.utils.Constant.RESULT_RECORD_DIALOG_HEIGHT
 
-fun Context.dialogWidthResize(dialog: Dialog, width: Float) {
+fun Context.dialogWidthResize(
+    dialog: Dialog,
+    widthRatio: Float,
+    isResultDialog: Boolean = false,
+) {
     val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
 
     if (Build.VERSION.SDK_INT < 30) {
@@ -16,15 +21,29 @@ fun Context.dialogWidthResize(dialog: Dialog, width: Float) {
         display.getSize(size)
 
         val window = dialog.window
-        val x = (size.x * width).toInt()
-
-        window?.setLayout(x, ViewGroup.LayoutParams.WRAP_CONTENT)
+        val x = (size.x * widthRatio).toInt()
+        val height  = if(isResultDialog) {
+            this.convertPxToDp(RESULT_RECORD_DIALOG_HEIGHT)
+        } else {
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        }
+        window?.setLayout(x, height)
     } else {
         val rect = windowManager.currentWindowMetrics.bounds
 
         val window = dialog.window
-        val x = (rect.width() * width).toInt()
+        val x = (rect.width() * widthRatio).toInt()
 
-        window?.setLayout(x, ViewGroup.LayoutParams.WRAP_CONTENT)
+        val height  = if(isResultDialog) {
+            this.convertPxToDp(RESULT_RECORD_DIALOG_HEIGHT)
+        } else {
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        }
+        window?.setLayout(x, height)
     }
+}
+
+fun Context.convertPxToDp(px: Int): Int {
+    val density = resources.displayMetrics.density
+    return (px * density).toInt()
 }
