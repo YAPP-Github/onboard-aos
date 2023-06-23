9d7098efdd11591502c8eb1a31341975da4a77f4
jaehong <83493143+prk4224@users.noreply.github.com>
2023-06-23 10:40:25 +0900

    refactor: ktlint 수정

--- presentation/src/main/java/com/yapp/bol/presentation/view/match/member_select/MemberSelectFragment.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/match/member_select/MemberSelectFragment.kt
@@ -46,7 +46,7 @@ class MemberSelectFragment : Fragment() {
     private val guestAddDialog by lazy {
         GuestAddDialog(
             context = requireContext(),
-            addGuest = {  },
+            addGuest = { },
             getValidateNickName = { nickname -> memberSelectViewModel.getValidateNickName(10, nickname) },
         )
     }
