38bb5fe5c7f8179ab9124f0c7cf75f6184b392af
EunJung Jung <agjjung@naver.com>
2023-06-23 22:04:56 +0900

    BOL-62 feat: group list search activity, fragment 구현

--- presentation/src/main/java/com/yapp/bol/presentation/view/group/search/GroupSearchFragment.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/group/search/GroupSearchFragment.kt
@@ -1,61 +1,116 @@
 package com.yapp.bol.presentation.view.group.search
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.widget.ImageButton
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.PagingData
 import com.yapp.bol.presentation.R
+import com.yapp.bol.presentation.base.BaseFragment
+import com.yapp.bol.presentation.databinding.FragmentGroupSearchBinding
+import com.yapp.bol.presentation.utils.loseFocusOnAction
+import com.yapp.bol.presentation.utils.textChangesToFlow
+import com.yapp.bol.presentation.utils.withLoadStateAdapters
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+@AndroidEntryPoint
+class GroupSearchFragment : BaseFragment<FragmentGroupSearchBinding>(R.layout.fragment_group_search) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [GroupSearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class GroupSearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val viewModel: GroupSearchViewModel by activityViewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onViewCreatedAction() {
+        super.onViewCreatedAction()
+        binding.lifecycleOwner = this@GroupSearchFragment
+        binding.viewGroupListLoading.root.visibility = View.GONE
+        bindView()
+    }
+
+    private fun bindView() {
+        val adapter = GroupListAdapter()
+        initPaging(adapter)
+        binding.initSearchView(adapter)
+        adapter.addOnPagesUpdatedListener {
+            binding.viewGroupListLoading.root.visibility = View.GONE
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_group_search, container, false)
+    private fun initPaging(adapter: GroupListAdapter) {
+        val concatAdapter = adapter.withLoadStateAdapters(
+            header = GroupListLoadStateAdapter { adapter.retry() },
+            footer = GroupListLoadStateAdapter { adapter.retry() }
+        )
+        binding.rvGroupList.adapter = concatAdapter
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GroupSearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            GroupSearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    // 상단 search view 초기화 관련 작업
+    private fun FragmentGroupSearchBinding.initSearchView(adapter: GroupListAdapter) {
+        binding.initEditText(adapter)
+
+        val editText = viewGroupSearch.etGroupSearch
+        val rightDrawableBtn = viewGroupSearch.btnSearchRight
+
+        rightDrawableBtn.setOnClickListener {
+            if (!editText.text.isNullOrEmpty() || !editText.text.isNullOrBlank()) {
+                editText.text.clear()
             }
+        }
+
+        editText.loseFocusOnAction(EditorInfo.IME_ACTION_SEARCH, this.root.context)
+
+        binding.viewGroupSearch.btnCreateGroup.setOnClickListener {
+            // TODO : create transition
+        }
     }
+
+    // search view의 edittext 세팅
+    private fun FragmentGroupSearchBinding.initEditText(adapter: GroupListAdapter) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            val editTextFlow = viewGroupSearch.etGroupSearch.textChangesToFlow()
+            val debounceDuration = 500L
+
+            editTextFlow
+                .onEach {
+                    val isTyping = !(it.isNullOrBlank() || it.isEmpty())
+                    viewGroupSearch.btnSearchRight.setImageButtonByState(isTyping)
+                }
+                .launchIn(this)
+
+            editTextFlow
+                .debounce(debounceDuration)
+                .onEach {
+                    adapter.searchByKeyword(it.toString())
+                }
+                .launchIn(this)
+        }
+    }
+
+    private fun GroupListAdapter.searchByKeyword(keyword: String) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            submitData(PagingData.empty())
+            viewModel.searchGroup(keyword).collectLatest {
+                submitData(it)
+            }
+        }
+    }
+
+    // search view의 edittext typing 여부에 따른 우측 아이콘 변경
+    private fun ImageButton.setImageButtonByState(isTyping: Boolean) =
+        viewLifecycleOwner.lifecycleScope.launch {
+            when (isTyping) {
+                true -> setImageDrawable(
+                    AppCompatResources.getDrawable(binding.root.context, R.drawable.ic_x)
+                )
+                false -> setImageDrawable(
+                    AppCompatResources.getDrawable(binding.root.context, R.drawable.ic_search)
+                )
+            }
+        }
+
 }
