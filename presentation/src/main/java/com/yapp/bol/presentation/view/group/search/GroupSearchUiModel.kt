15b81fb55152a94e987829745c747b1b4de28c6c
EunJung Jung <agjjung@naver.com>
2023-06-27 11:19:46 +0900

    BOL-61 refactor: rename 된 group ui model 관련 중복 파일들 제거

--- presentation/src/main/java/com/yapp/bol/presentation/view/group/search/GroupSearchUiModel.kt
+++
@@ -1,8 +0,0 @@
-package com.yapp.bol.presentation.view.group.search
-
-import com.yapp.bol.domain.model.GroupSearchItem
-
-sealed class GroupSearchUiModel {
-    data class GroupList(val groupSearchItem: GroupSearchItem) : GroupSearchUiModel()
-    data class DataNotFound(val keyword: String) : GroupSearchUiModel()
-}
