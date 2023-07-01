f7ff9562415bb9707d03420b521b52717d35e7f2
EunJung Jung <agjjung@naver.com>
2023-07-02 03:36:10 +0900

    BOL-122 feat: ranking - game list UseCase 구현

---
+++ domain/src/main/java/com/yapp/bol/domain/usecase/rank/GetUserRankGameListUseCase.kt
@@ -0,0 +1,13 @@
+package com.yapp.bol.domain.usecase.rank
+
+import com.yapp.bol.domain.model.ApiResult
+import com.yapp.bol.domain.model.GameItem
+import com.yapp.bol.domain.repository.Repository
+import kotlinx.coroutines.flow.Flow
+
+class GetUserRankGameListUseCase(
+    private val repository: Repository
+) {
+    operator fun invoke(groupId: Int): Flow<ApiResult<List<GameItem>>> =
+        repository.getGameList(groupId = groupId)
+}
