b2706ac8f8fd473812d8fd7b43005ca5c353ecaa
EunJung Jung <agjjung@naver.com>
2023-07-04 15:19:24 +0900

    BOL-122 feat: rank game item adapter 수정

--- presentation/src/main/java/com/yapp/bol/presentation/view/home/rank/UserRankGameViewHolder.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/home/rank/UserRankGameViewHolder.kt
@@ -1,5 +1,7 @@
 package com.yapp.bol.presentation.view.home.rank
 
+import android.content.Context
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,20 +9,86 @@ import com.yapp.bol.domain.model.GameItem
 import com.yapp.bol.presentation.databinding.ItemRankGameListBinding
 import com.yapp.bol.presentation.utils.loadImage
 
-class UserRankGameViewHolder(private val binding: ItemRankGameListBinding) : RecyclerView.ViewHolder(binding.root) {
+class UserRankGameViewHolder(
+    private val binding: ItemRankGameListBinding,
+    private val onClick: (Int) -> Unit,
+) : RecyclerView.ViewHolder(binding.root) {
 
-    fun bind(gameItem: GameItem) {
+    init {
+        binding.viewGame.root.setOnClickListener {
+            onClick.invoke(layoutPosition)
+            binding.setGameItemLayoutParamsWhenSelected()
+        }
+    }
+
+    fun bind(gameItem: GameItem, isSelected: Boolean) {
+        showGameItemData(gameItem)
+        showGameItemBySelectState(isSelected)
+    }
+
+    private fun showGameItemData(gameItem: GameItem) {
         binding.apply {
             binding.viewGame.tvGame.text = gameItem.name
             binding.viewGame.ivGame.loadImage(gameItem.img)
         }
     }
 
+    private fun showGameItemBySelectState(isSelected: Boolean) {
+        if (isSelected) {
+            binding.setGameItemLayoutParamsWhenSelected()
+        } else {
+            binding.setGameItemLayoutParamsWhenUnselected()
+        }
+    }
+
+    private fun ItemRankGameListBinding.setGameItemLayoutParamsWhenSelected() {
+        val ivGameLP = viewGame.ivGame.layoutParams.apply {
+            val px = dpToPx(
+                binding.root.context, 68f
+            )
+            width = px
+            height = px
+        }
+        viewGame.ivGame.layoutParams = ivGameLP
+
+        val paddingLP = viewStartPadding.layoutParams.apply {
+            val px = dpToPx(binding.root.context, 11f)
+            width = px
+        }
+        binding.viewStartPadding.layoutParams = paddingLP
+        binding.viewEndPadding.layoutParams = paddingLP
+    }
+
+    private fun ItemRankGameListBinding.setGameItemLayoutParamsWhenUnselected() {
+        val ivGameLP = viewGame.ivGame.layoutParams
+        ivGameLP.apply {
+            val px = dpToPx(
+                binding.root.context, 52f
+            )
+            width = px
+            height = px
+        }
+        viewGame.ivGame.layoutParams = ivGameLP
+
+        val paddingLP = viewStartPadding.layoutParams
+        paddingLP.apply {
+            val px = dpToPx(binding.root.context, 7f)
+            width = px
+        }
+        viewStartPadding.layoutParams = paddingLP
+        viewEndPadding.layoutParams = paddingLP
+    }
+
+    private fun dpToPx(context: Context, dp: Float): Int {
+        val displayMetrics = context.resources.displayMetrics
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, displayMetrics).toInt()
+    }
+
     companion object {
-        fun create(parent: ViewGroup): UserRankGameViewHolder {
+        fun create(parent: ViewGroup, onClick: (Int) -> Unit): UserRankGameViewHolder {
             val inflater = LayoutInflater.from(parent.context)
             val binding = ItemRankGameListBinding.inflate(inflater, parent, false)
-            return UserRankGameViewHolder(binding)
+            return UserRankGameViewHolder(binding, onClick)
         }
     }
 }
