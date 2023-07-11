9d153e7c6c0df727b9bab37731736a5a503e6b2b
jaehong <83493143+prk4224@users.noreply.github.com>
2023-07-11 09:26:30 +0900

    Refactor: ktlint 수정

--- presentation/src/main/java/com/yapp/bol/presentation/view/login/LoginActivity.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/login/LoginActivity.kt
@@ -3,7 +3,6 @@ package com.yapp.bol.presentation.view.login
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import com.kakao.sdk.auth.Constants.ACCESS_TOKEN
 import com.yapp.bol.presentation.R
 import com.yapp.bol.presentation.databinding.ActivityMainBinding
