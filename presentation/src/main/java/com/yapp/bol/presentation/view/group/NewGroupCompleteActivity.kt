587dbca9b194df12eb958076e6d2bea7e0f5850e
EunJung Jung <agjjung@naver.com>
2023-07-12 20:38:58 +0900

    BOL-122 refactor: copy to clipboard 시 toast util 함수로 Refactoring

--- presentation/src/main/java/com/yapp/bol/presentation/view/group/NewGroupCompleteActivity.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/group/NewGroupCompleteActivity.kt
@@ -9,8 +9,10 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.yapp.bol.domain.model.NewGroupItem
+import com.yapp.bol.presentation.R
 import com.yapp.bol.presentation.databinding.ActivityNewGroupCompleteBinding
 import com.yapp.bol.presentation.utils.isInputTextValid
+import com.yapp.bol.presentation.utils.showToast
 import com.yapp.bol.presentation.view.group.NewGroupActivity.Companion.ACCESS_CODE_KEY
 import com.yapp.bol.presentation.view.match.MatchActivity
 
@@ -70,11 +72,10 @@ class NewGroupCompleteActivity : AppCompatActivity() {
         val clipboard: ClipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
         val clip = ClipData.newPlainText(COPY_ACCESS_CODE_KEY, accessCode)
         clipboard.setPrimaryClip(clip)
-        Toast.makeText(this, COPY_SUCCESS_MESSAGE, Toast.LENGTH_SHORT).show()
+        binding.root.context.showToast(R.string.copy_access_code, Toast.LENGTH_SHORT)
     }
 
     companion object {
         const val COPY_ACCESS_CODE_KEY = "AccessCode"
-        const val COPY_SUCCESS_MESSAGE = "참여 코드가 복사되었습니다."
     }
 }
