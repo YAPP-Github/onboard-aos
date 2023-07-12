f6295be0edc9df690a253585522951a563d1aa1d
EunJung Jung <agjjung@naver.com>
2023-07-12 20:56:46 +0900

    BOL-122 feat: system ui(상태바, 네비게이션바) 변경하는 유틸 함수 구현

--- presentation/src/main/java/com/yapp/bol/presentation/utils/SystemUi.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/utils/SystemUi.kt
@@ -4,11 +4,21 @@ import android.app.Activity
 import android.view.WindowManager
 import androidx.core.view.WindowInsetsControllerCompat
 
+fun setSystemUi(activity: Activity, color: Int) {
+    setStatusBarColor(activity, color)
+    setNavigationBarColor(activity, color)
+}
 
 fun setStatusBarColor(activity: Activity, color: Int) {
     activity.window.apply {
         addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         statusBarColor = color
         WindowInsetsControllerCompat(this, this.decorView).isAppearanceLightStatusBars = false
+    }
+}
+
+fun setNavigationBarColor(activity: Activity, color: Int) {
+    activity.window.apply {
+        navigationBarColor = color
     }
 }
