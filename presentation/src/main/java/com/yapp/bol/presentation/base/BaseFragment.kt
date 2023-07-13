c8a18148d0c89be5b80c38793950c4e2e78a2760
jaehong <83493143+prk4224@users.noreply.github.com>
2023-07-14 08:45:10 +0900

    Refactor: BaseFragment 수정
    Viewbinding 제거

--- presentation/src/main/java/com/yapp/bol/presentation/base/BaseFragment.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/base/BaseFragment.kt
@@ -9,9 +9,9 @@ import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import androidx.viewbinding.ViewBindings
 
-abstract class BaseFragment<T>(@LayoutRes private val layoutRes: Int) :
-    Fragment() where T : ViewDataBinding, T : ViewBinding {
+abstract class BaseFragment<T : ViewDataBinding>(@LayoutRes private val layoutRes: Int) : Fragment() {
 
     private var _binding: T? = null
     val binding: T
