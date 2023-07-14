0878096965c6146cd4d9f452616cc4a5fb0da83c
KyeongMIn <goodluckplz820@gmail.com>
2023-07-14 20:59:35 +0900

    BOL-25 refactor : ktlint 수정
    - unused import

--- presentation/src/main/java/com/yapp/bol/presentation/viewmodel/login/LoginViewModel.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/viewmodel/login/LoginViewModel.kt
@@ -8,9 +8,7 @@ import com.yapp.bol.domain.usecase.auth.SaveRefreshTokenUseCase
 import com.yapp.bol.domain.usecase.login.LoginUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
