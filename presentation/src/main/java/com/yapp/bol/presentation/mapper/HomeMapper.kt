d3f0a5018f1ca6c8f3d87a1a3fafbe985d6368f0
EunJung Jung <agjjung@naver.com>
2023-07-21 07:10:57 +0900

    BOL-122 feat: home - user rank - rank nullable로 변경하며 mapper 방식 변경

--- presentation/src/main/java/com/yapp/bol/presentation/mapper/HomeMapper.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/mapper/HomeMapper.kt
@@ -10,16 +10,18 @@ object HomeMapper {
     fun UserRankListItem.toUserRankUiModel(): List<UserRankUiModel> {
         val userRank1To3 = mutableListOf<UserRankItem>()
         val userRankAfter4 = mutableListOf<UserRankUiModel>()
+        val resultList = mutableListOf<UserRankUiModel>()
 
-        userRankItemList.map { item ->
-            if (item.rank <= HomeConfig.USER_RV_1_TO_3_UI_RANK_THRESHOLD) {
-                userRank1To3.add(item)
+        if (userRankItemList.size <= HomeConfig.NO_RANK_THRESHOLD) { return resultList }
+
+        userRankItemList.forEachIndexed { index, userRankItem ->
+            if (index < HomeConfig.USER_RV_1_TO_3_UI_RANK_THRESHOLD) {
+                userRank1To3.add(userRankItem)
             } else {
-                userRankAfter4.add(UserRankUiModel.UserRankAfter4(item))
+                userRankAfter4.add(UserRankUiModel.UserRankAfter4(userRankItem))
             }
         }
 
-        val resultList = mutableListOf<UserRankUiModel>()
         resultList.add(UserRankUiModel.UserRank1to3(userRank1To3))
         resultList.addAll(userRankAfter4)
 
