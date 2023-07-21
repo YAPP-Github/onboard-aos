bfdc68d725dcf1787c97046243feb3179180b6a6
EunJung Jung <agjjung@naver.com>
2023-07-21 15:57:16 +0900

    BOL-122 feat: setting - term ui 구현

--- presentation/src/main/java/com/yapp/bol/presentation/view/setting/TermFragment.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/setting/TermFragment.kt
@@ -1,20 +1,35 @@
 package com.yapp.bol.presentation.view.setting
 
+import androidx.navigation.findNavController
 import com.yapp.bol.presentation.R
 import com.yapp.bol.presentation.base.BaseFragment
 import com.yapp.bol.presentation.databinding.FragmentTermBinding
+import com.yapp.bol.presentation.utils.config.SettingConfig
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TermFragment : BaseFragment<FragmentTermBinding>(R.layout.fragment_term) {
 
     override fun onViewCreatedAction() {
         super.onViewCreatedAction()
 
-        setTitle(false)
+        val isPrivacyTerm: Boolean = arguments?.getBoolean(SettingConfig.TERMS_BUNDLE_KEY) ?: kotlin.run {
+            true
+        }
+
+        setTitle(isPrivacyTerm)
+        setBackButton()
     }
 
     private fun setTitle(isPrivacyTerm: Boolean) {
         binding.tvTitle.text = if (isPrivacyTerm) {
             binding.root.context.getString(R.string.setting_terms_privacy)
         } else { binding.root.context.getString(R.string.setting_terms_service) }
+    }
+
+    private fun setBackButton() {
+        binding.btnBack.setOnClickListener {
+            binding.root.findNavController().popBackStack()
+        }
     }
 }
