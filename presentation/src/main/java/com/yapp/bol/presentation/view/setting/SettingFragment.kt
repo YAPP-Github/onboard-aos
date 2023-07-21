2a70742ec1daffc30efe7fd108fad62d36cc207c
EunJung Jung <agjjung@naver.com>
2023-07-21 13:31:35 +0900

    BOL-122 feat: setting 화면 구현

---
+++ presentation/src/main/java/com/yapp/bol/presentation/view/setting/SettingFragment.kt
@@ -0,0 +1,11 @@
+package com.yapp.bol.presentation.view.setting
+
+import com.yapp.bol.presentation.R
+import com.yapp.bol.presentation.base.BaseFragment
+import com.yapp.bol.presentation.databinding.FragmentSettingBinding
+
+class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
+    override fun onViewCreatedAction() {
+        super.onViewCreatedAction()
+    }
+}
