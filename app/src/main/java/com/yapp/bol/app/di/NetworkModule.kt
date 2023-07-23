ce62123989165ddaff8219f4fae27223fd6f14d5
jaehong <83493143+prk4224@users.noreply.github.com>
2023-07-23 17:03:12 +0900

    refactor: ktlint 수정

--- app/src/main/java/com/yapp/bol/app/di/NetworkModule.kt
+++ app/src/main/java/com/yapp/bol/app/di/NetworkModule.kt
@@ -15,7 +15,6 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -92,7 +91,6 @@ object NetworkModule {
     fun provideMatchApiService(retrofit: Retrofit): MatchApi {
         return retrofit.create(MatchApi::class.java)
     }
-
 
     @Provides
     @Singleton
