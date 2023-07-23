81c9eecd50b6228c098bc725a14b4b36e534c45f
jaehong <83493143+prk4224@users.noreply.github.com>
2023-07-23 16:31:48 +0900

    refactor: merge conflict 해결

--- app/src/main/java/com/yapp/bol/app/di/NetworkModule.kt
+++ app/src/main/java/com/yapp/bol/app/di/NetworkModule.kt
@@ -5,6 +5,7 @@ import com.yapp.bol.data.remote.GroupApi
 import com.yapp.bol.data.remote.ImageFileApi
 import com.yapp.bol.data.remote.LoginApi
 import com.yapp.bol.data.remote.MatchApi
+import com.yapp.bol.data.remote.UserApi
 import com.yapp.bol.data.utils.Utils.BASE_URL
 import dagger.Module
 import dagger.Provides
@@ -90,5 +91,12 @@ object NetworkModule {
     @Singleton
     fun provideMatchApiService(retrofit: Retrofit): MatchApi {
         return retrofit.create(MatchApi::class.java)
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideUserApiService(retrofit: Retrofit): UserApi {
+        return retrofit.create(UserApi::class.java)
     }
 }
