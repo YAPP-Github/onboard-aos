055eb531b0b30379990167017f1b6139d16582d8
EunJung Jung <agjjung@naver.com>
2023-07-27 01:45:34 +0900

    BOL-264 style: ktlint formatting

--- presentation/src/main/java/com/yapp/bol/presentation/view/login/SplashActivity.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/login/SplashActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.viewModels
 import com.yapp.bol.presentation.R
 import com.yapp.bol.presentation.base.BaseActivity
 import com.yapp.bol.presentation.databinding.ActivitySplashBinding
-import com.yapp.bol.presentation.view.group.search.GroupSearchActivityTest
 import com.yapp.bol.presentation.view.home.HomeActivity
-import com.yapp.bol.presentation.view.match.MatchActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,13 +19,13 @@ class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_spl
 
     private fun subscribeObservables() {
         splashViewModel.animationState.observe(this) {
-            if(it.not()) return@observe
+            if (it.not()) return@observe
             splashViewModel.getMyGroupList()
         }
 
         splashViewModel.myGroupList.observe(this) {
-            if(it == null) return@observe
-            val target = if(it.isEmpty()) LoginActivity::class.java else HomeActivity::class.java
+            if (it == null) return@observe
+            val target = if (it.isEmpty()) LoginActivity::class.java else HomeActivity::class.java
             startActivity(Intent(this, target))
             finish()
         }
