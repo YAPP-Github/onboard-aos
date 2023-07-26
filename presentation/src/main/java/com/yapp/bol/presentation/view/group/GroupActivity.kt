0a546c4cdc2b7f6a930dfc3b0617e96ac6e27e54
EunJung Jung <100047095+eunjjungg@users.noreply.github.com>
2023-07-26 17:35:11 +0900

    [BOL-278] Fix Group Flow Error  (#89)
    * BOL-278 feat: group nav graph 추가
    
    * BOL-278 feat: group nav host 맞춰서 전체 플로우 연결
    
    * BOL-278 style: ktlint formatting

--- presentation/src/main/java/com/yapp/bol/presentation/view/group/GroupActivity.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/group/GroupActivity.kt
@@ -1,21 +1,14 @@
 package com.yapp.bol.presentation.view.group
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import com.yapp.bol.presentation.R
+import com.yapp.bol.presentation.base.BaseActivity
 import com.yapp.bol.presentation.databinding.ActivityGroupBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class GroupActivity : AppCompatActivity() {
+class GroupActivity : BaseActivity<ActivityGroupBinding>(R.layout.activity_group) {
 
-    val binding: ActivityGroupBinding by lazy {
-        DataBindingUtil.setContentView(this, R.layout.activity_group)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+    override fun onCreateAction() {
+        super.onCreateAction()
     }
 }
