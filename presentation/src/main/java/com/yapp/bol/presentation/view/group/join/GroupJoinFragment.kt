ac6c4bb0fa52767282b25becf6dec3d31cce03b0
차경민 <68880067+ckrudals@users.noreply.github.com>
2023-07-27 20:13:34 +0900

    feat [BOL-227] : 닉네임 검사 기능 추가 (#80)
    * feat [BOL-227] : 닉네임 검사 기능 추가
    
    * BOL-227 refactor : 겹치는 파일 develop 에 있는 것으로 수정
    
    * BOL-227 refactor : ktlint 수정
    
    * BOL-227 refactor : ktlint 수정
    
    * BOL-227 refactor : ktlint 수정
    
    * BOL-227 refactor : ktlint 수정

--- presentation/src/main/java/com/yapp/bol/presentation/view/group/join/GroupJoinFragment.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/group/join/GroupJoinFragment.kt
@@ -84,8 +84,6 @@ class GroupJoinFragment : Fragment() {
                     margin = Margin(rightMargin = context.dpToPx(8)),
                 )
                 .setOnLimit { code, dialog ->
-                    viewModel.checkGroupJoinByAccessCode(code)
-
                     viewModel.successCheckGroupAccessCode.collectWithLifecycle(viewLifecycleOwner) { (success, message) -> // ktlint-disable max-line-length
                         if (success) {
                             showProfileSettingDialog(code)
@@ -130,6 +128,9 @@ class GroupJoinFragment : Fragment() {
             loading.collectWithLifecycle(viewLifecycleOwner) { (isLoading, message) ->
                 binding.loadingLayout.isVisible = isLoading
                 binding.tvLoadingTitle.text = message
+            }
+            groupItem.collectWithLifecycle(viewLifecycleOwner) {
+                binding.groupAdminView.setGroupItemDetailTitle(it?.ownerNickname.orEmpty())
             }
         }
     }
