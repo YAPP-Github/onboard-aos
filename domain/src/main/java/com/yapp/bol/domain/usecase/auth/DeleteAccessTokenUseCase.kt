ad0109eb068caba7e2a435142ba6c8da25a4b831
EunJung Jung <agjjung@naver.com>
2023-07-29 22:23:59 +0900

    BOL-267 fix: UseCase에 inject 키워드 추가

--- domain/src/main/java/com/yapp/bol/domain/usecase/auth/DeleteAccessTokenUseCase.kt
+++ domain/src/main/java/com/yapp/bol/domain/usecase/auth/DeleteAccessTokenUseCase.kt
@@ -1,8 +1,9 @@
 package com.yapp.bol.domain.usecase.auth
 
 import com.yapp.bol.domain.repository.AuthRepository
+import javax.inject.Inject
 
-class DeleteAccessTokenUseCase(
+class DeleteAccessTokenUseCase @Inject constructor (
     private val repository: AuthRepository,
 ) {
     suspend operator fun invoke() = repository.deleteAccessToken()
--- domain/src/main/java/com/yapp/bol/domain/usecase/auth/DeleteRefreshTokenUseCase.kt
+++ domain/src/main/java/com/yapp/bol/domain/usecase/auth/DeleteRefreshTokenUseCase.kt
@@ -1,8 +1,9 @@
 package com.yapp.bol.domain.usecase.auth
 
 import com.yapp.bol.domain.repository.AuthRepository
+import javax.inject.Inject
 
-class DeleteRefreshTokenUseCase(
+class DeleteRefreshTokenUseCase @Inject constructor (
     private val repository: AuthRepository,
 ) {
     suspend operator fun invoke() = repository.deleteRefreshToken()
