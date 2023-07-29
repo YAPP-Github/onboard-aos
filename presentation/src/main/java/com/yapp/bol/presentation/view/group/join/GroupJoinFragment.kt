e94c57ebb6cd61f01fe13c8693eb28fea3cd6f70
KyeongMIn <goodluckplz820@gmail.com>
2023-07-29 15:38:33 +0900

    BOL-295 refactor : statusbar 가 view 를 침범하는 이슈 해결

--- presentation/src/main/java/com/yapp/bol/presentation/view/group/join/GroupJoinFragment.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/group/join/GroupJoinFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.ui.graphics.Color
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
--- presentation/src/main/java/com/yapp/bol/presentation/view/home/HomeActivity.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/home/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.yapp.bol.presentation.view.home
 import com.yapp.bol.presentation.R
 import com.yapp.bol.presentation.base.BaseActivity
 import com.yapp.bol.presentation.databinding.ActivityHomeBinding
+import com.yapp.bol.presentation.utils.setStatusBarColor
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
