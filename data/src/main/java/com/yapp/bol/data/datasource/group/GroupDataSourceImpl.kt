4abf6ac6bc6d7e1743859f0e576aabeb8ceb2632
EunJung Jung <agjjung@naver.com>
2023-08-13 02:07:16 +0900

    BOL-375 feat: GroupDataSource GroupApi 맞춰서 구현

--- data/src/main/java/com/yapp/bol/data/datasource/group/GroupDataSourceImpl.kt
+++ data/src/main/java/com/yapp/bol/data/datasource/group/GroupDataSourceImpl.kt
@@ -1,8 +1,13 @@
 package com.yapp.bol.data.datasource.group
 
-import com.yapp.bol.data.model.group.response.GroupSearchApiResponse
+import com.yapp.bol.data.model.group.request.CheckGroupJonByAccessCodeRequest
+import com.yapp.bol.data.model.group.request.NewGroupApiRequest
+import com.yapp.bol.data.model.group.response.CheckGroupJoinByAccessCodeResponse
 import com.yapp.bol.data.model.group.response.GroupDetailResponse
-import com.yapp.bol.data.model.group.response.JoinedGroupResponse
+import com.yapp.bol.data.model.group.response.GroupSearchApiResponse
+import com.yapp.bol.data.model.group.response.NewGroupApiResponse
+import com.yapp.bol.data.model.group.response.RandomImageResponse
+import com.yapp.bol.data.model.rank.UserRankApiResponse
 import com.yapp.bol.data.remote.GroupApi
 import com.yapp.bol.domain.handle.BaseRepository
 import com.yapp.bol.domain.model.ApiResult
@@ -13,6 +18,19 @@ import javax.inject.Inject
 class GroupDataSourceImpl @Inject constructor(
     private val groupApi: GroupApi,
 ) : BaseRepository(), GroupDataSource {
+
+    override fun postCreateGroup(
+        name: String,
+        description: String,
+        organization: String,
+        imageUrl: String,
+        nickname: String
+    ): Flow<ApiResult<NewGroupApiResponse>> = flow {
+        val result = safeApiCall {
+            groupApi.postOAuthApi(NewGroupApiRequest(name, description, organization, imageUrl, nickname))
+        }
+        emit(result)
+    }
 
     override suspend fun searchGroup(name: String, page: Int, pageSize: Int):
         ApiResult<GroupSearchApiResponse> {
@@ -25,12 +43,10 @@ class GroupDataSourceImpl @Inject constructor(
         }
     }
 
-    override fun getJoinedGroup(): Flow<ApiResult<JoinedGroupResponse>> =
-        flow {
-            safeApiCall {
-                groupApi.getJoinedGroup()
-            }.also { emit(it) }
-        }
+    override fun getRandomImage(): Flow<ApiResult<RandomImageResponse>> = flow {
+        val result = safeApiCall { groupApi.getRandomImage() }
+        emit(result)
+    }
 
     override fun getGroupDetail(groupId: Long): Flow<ApiResult<GroupDetailResponse>> =
         flow {
@@ -38,4 +54,26 @@ class GroupDataSourceImpl @Inject constructor(
                 groupApi.getGroupDetail(groupId)
             }.also { emit(it) }
         }
+
+    override fun getUserRank(groupId: Int, gameId: Int): Flow<ApiResult<UserRankApiResponse>> =
+        flow {
+            safeApiCall {
+                groupApi.getUserRank(
+                    groupId = groupId,
+                    gameId = gameId
+                )
+            }.also { emit(it) }
+        }
+
+    override fun checkGroupJoinAccessCode(
+        groupId: String,
+        accessCode: String,
+    ): Flow<ApiResult<CheckGroupJoinByAccessCodeResponse>> {
+        return flow {
+            val result = safeApiCall {
+                groupApi.checkGroupJoinAccessCode(groupId, CheckGroupJonByAccessCodeRequest(accessCode))
+            }
+            emit(result)
+        }
+    }
 }
