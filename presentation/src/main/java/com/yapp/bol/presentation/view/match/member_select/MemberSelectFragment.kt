b4baa1b785e0a25fc26c9688b55b7ec25db4dfe9
jaehong <83493143+prk4224@users.noreply.github.com>
2023-09-12 10:26:50 +0900

    Refactor: 코드 정렬

--- presentation/src/main/java/com/yapp/bol/presentation/view/match/member_select/MemberSelectFragment.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/match/member_select/MemberSelectFragment.kt
@@ -147,7 +147,7 @@ class MemberSelectFragment : BaseFragment<FragmentMemberSelectBinding>(R.layout.
     }
 
     private fun setSearchResultNothing(visible: Boolean, keyword: String) = with(binding) {
-        if(keyword.isEmpty()) return@with
+        if (keyword.isEmpty()) return@with
         val searchResult = String.format(resources.getString(R.string.search_result_nothing), keyword)
         tvSearchResultNothing.apply {
             text = searchResult
