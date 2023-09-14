76e71488971481373f0ce8ab070f53ed7e9c33a5
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-09-14 11:34:39 +0900

    [BOL-386]  매치 화면 멤버선택 진입 시 empty 화면 잠깐 보이는 버그 수정 (#146)
    * Fix: 매치 화면 멤버선택 진입 시 empty 화면 잠깐 보이는 버그 수정
    
    * Refactor: 코드 정렬

--- presentation/src/main/java/com/yapp/bol/presentation/view/match/member_select/MemberSelectFragment.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/match/member_select/MemberSelectFragment.kt
@@ -162,15 +162,12 @@ class MemberSelectFragment : BaseFragment<FragmentMemberSelectBinding>(R.layout.
     }
 
     private fun setSearchResultNothing(visible: Boolean, keyword: String) = with(binding) {
+        if (keyword.isEmpty()) return@with
         val searchResult = String.format(resources.getString(R.string.search_result_nothing), keyword)
-        viewSearchResultNothing.isVisible = visible
-        tvSearchResultNothingGuide.isVisible = visible
-        btnGuestAddNothing.isVisible = visible
-        ivPlusNothing.isVisible = visible
         tvSearchResultNothing.apply {
             text = searchResult
-            isVisible = visible
         }
+        emptyGroup.isVisible = visible
     }
 
     private fun setPlayerRangeText(minPlayer: Int, maxPlayer: Int) {
