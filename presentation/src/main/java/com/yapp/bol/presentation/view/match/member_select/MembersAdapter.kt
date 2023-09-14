421a0d6cb6fc1ab8495d4c6b714e5b4e4dc417ba
JaeHong Park <83493143+prk4224@users.noreply.github.com>
2023-09-14 11:34:11 +0900

    [BOL-332] 매치 기록 화면 Empty Member 이미지 크기 수정 (#145)
    * Refactor: 주사위 크기 수정
    
    * Refactor: lint 수정
    
    * Refactor: 변수 이름 변경 image -> imageRes

--- presentation/src/main/java/com/yapp/bol/presentation/view/match/member_select/MembersAdapter.kt
+++ presentation/src/main/java/com/yapp/bol/presentation/view/match/member_select/MembersAdapter.kt
@@ -43,8 +43,14 @@ class MembersAdapter(
         }
 
         private fun setImageView(item: MemberInfo) {
-            val image = if (item.role == GUEST) DR.drawable.img_dice_empty_small else DR.drawable.img_dice
-            binding.ivMemberLevelIcon.setImageResource(image)
+            val imageRes = if (item.role == GUEST) {
+                binding.ivMemberLevelIcon.setPadding(7, 7, 7, 7)
+                DR.drawable.img_dice_empty
+            } else {
+                binding.ivMemberLevelIcon.setPadding(0, 0, 0, 0)
+                DR.drawable.img_dice
+            }
+            binding.ivMemberLevelIcon.setImageResource(imageRes)
         }
 
         private fun setClickListener(item: MemberInfo, position: Int) {
