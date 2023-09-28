c086c826d190b58516d822c4af76ac308b2c4f3a
jaehong <83493143+prk4224@users.noreply.github.com>
2023-09-28 14:43:21 +0900

    feat: 주사위 이미지 변경 함수 Util로 변경

---
+++ presentation/src/main/java/com/yapp/bol/presentation/utils/config/ImageViewUtil.kt
@@ -0,0 +1,17 @@
+package com.yapp.bol.presentation.utils.config
+
+import android.widget.ImageView
+import com.yapp.bol.designsystem.R
+import com.yapp.bol.presentation.view.match.MatchActivity
+
+
+fun ImageView.setDice(role: String) {
+    val imageRes = if (role == MatchActivity.GUEST) {
+        setPadding(10, 10, 10, 10)
+        R.drawable.img_dice_empty
+    } else {
+        setPadding(0, 0, 0, 0)
+        R.drawable.img_dice
+    }
+    setImageResource(imageRes)
+}
